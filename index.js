ca8478928b5b4829e44090077c45d69cb1ea55fa
Marco <marcovelardejr@gmail.com>
2022-05-07 20:31:42 -0500

    commit tree 59cb3f1bed191a430ffec6fa58e3acabc955777d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah','Bobby']
+
+function findMatching (drivers, string){
+    let Task1 = drivers.filter(element => element.toUpperCase() === string.toUpperCase())
+    return Task1
+}
+
+
+  function fuzzyMatch(drivers, letters) {
+      let Task2 = drivers.filter(element => element.startsWith(letters))
+      return Task2
+  }
+
+  function matchName(drivers,string){
+      let Task3 = drivers.filter(element => element.name === string)
+      return Task3
+  }
